5928b198f53a9a3106a5d1be7ab1b71df5ec59ba
Astra-001 <60982780+Astra-001@users.noreply.github.com>
2022-12-01 14:46:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const ytdl = require('ytdl-core');
+const app = express();
+const readline = require('readline');
+const ffmpeg = require('fluent-ffmpeg');
+app.use('/static', express.static('./static'));
+
+
+app.get('/', (req, res) => { 
+    res.sendFile('index.html', { root: './' });
+});
+
+app.get('/download', (req, res) => {
+    var url = req.query.url;    
+
+    const stream = ytdl(url, {
+        quality: 'highestaudio',
+    });
+    
+    let start = Date.now();
+
+    ffmpeg(stream)
+    .audioBitrate(128)
+    .save(`static/audio1.mp3`)
+    .on('progress', p => {
+        readline.cursorTo(process.stdout, 0);
+        process.stdout.write(`${p.targetSize}kb downloaded`);
+    })
+    .on('end', () => {
+        console.log(`\ndone, thanks - ${(Date.now() - start) / 1000}s`);
+        res.redirect('/');
+    });
+});
+
+app.listen(3000, () => { 
+    console.log("sdjskdjsjdslkd");
+});
